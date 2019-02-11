f4b44b37835cc4a3ccccb129f44fec90980c9f6f
kimseasok <basicblogtalk@gmail.com>
2019-02-11 00:02:36 -0500

    Privide serivce for specific component

--- src/app/edit-reader/edit-reader.component.ts
+++ src/app/edit-reader/edit-reader.component.ts
@@ -4,24 +4,31 @@ import { ActivatedRoute } from '@angular/router';
 import { allReaders } from 'app/data';
 import { Reader } from 'app/models/reader';
 import { DataService } from 'app/services/data.service';
+import { BadgeService } from 'app/services/badge.service';
 
 @Component({
   selector: 'app-edit-reader',
   templateUrl: './edit-reader.component.html',
-  styles: []
+  styles: [],
+  providers: [BadgeService]
 })
 export class EditReaderComponent implements OnInit {
   selectedReader: Reader;
+  currentBadge: string;
 
   constructor(
     private route: ActivatedRoute,
-    private dataService: DataService
+    private dataService: DataService,
+    private badgeSerivce: BadgeService
   ) {}
 
   ngOnInit() {
     // tslint:disable-next-line:radix
     const readerID: number = parseInt(this.route.snapshot.params['id']);
     this.selectedReader = this.dataService.getReaderById(readerID);
+    this.currentBadge = this.badgeSerivce.getReaderBadge(
+      this.selectedReader.totalMinutesRead
+    );
   }
 
   saveChanges() {
