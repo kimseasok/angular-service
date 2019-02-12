3b7f3ce731226e8a542a7c0504582cb1b25bc61e
kimseasok <basicblogtalk@gmail.com>
2019-02-12 05:02:40 -0500

    fix resolver in the route

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,26 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { LoggerService } from './logger.service';
 import { DataService } from './data.service';
-import { PlainLoggerService } from "./plain-logger.service";
-import { throwIfAlreadyLoaded } from "app/core/module-import-guard";
+import { PlainLoggerService } from './plain-logger.service';
+import { throwIfAlreadyLoaded } from 'app/core/module-import-guard';
 import { BookTrackerErrorHandlerService } from './book-tracker-error-handler.service';
-
+import { BookResolverService } from './book-resolver.service';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   declarations: [],
   providers: [
-    LoggerService, 
-    DataService, 
+    LoggerService,
+    DataService,
+    BookResolverService,
     { provide: ErrorHandler, useClass: BookTrackerErrorHandlerService }
   ]
 })
 export class CoreModule {
-
-  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     throwIfAlreadyLoaded(parentModule, 'CoreModule');
   }
-  
- }
+}
