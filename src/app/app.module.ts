69f7f7a620288e6f83dd0295e437c5953192ac47
kimseasok <basicblogtalk@gmail.com>
2019-02-11 22:00:45 -0500

    Prepare for http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AddBookComponent } from './add-book/add-book.component';
 import { AddReaderComponent } from './add-reader/add-reader.component';
@@ -9,7 +11,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EditBookComponent } from './edit-book/edit-book.component';
 import { EditReaderComponent } from './edit-reader/edit-reader.component';
-import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -24,8 +25,9 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    CoreModule
+    CoreModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { allBooks, allReaders } from 'app/data';
-import { Reader } from "app/models/reader";
-import { Book } from "app/models/book";
+import { Reader } from 'app/models/reader';
+import { Book } from 'app/models/book';
 import { BookTrackerError } from 'app/models/bookTrackerError';
 
 @Injectable()
 export class DataService {
-
-  constructor() { }
-
   mostPopularBook: Book = allBooks[0];
+  constructor(private http: HttpClient) {}
 
   setMostPopularBook(popularBook: Book): void {
     this.mostPopularBook = popularBook;
@@ -30,5 +30,5 @@ export class DataService {
 
   getBookById(id: number): Book {
     return allBooks.find(book => book.bookID === id);
-  }  
+  }
 }
